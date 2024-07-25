c7a47d6e359f9a6d1bee725c987306af35fd027c
aalaaabdullah95 <aalaaabdullah835@gmail.com>
2024-07-25 04:28:21 +0300

    add note button sheet

---
+++ lib/View/notes_view.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/View/Widget/add_note_button_sheet.dart';
+
+import 'package:notes_app/View/Widget/note_view_body.dart';
+
+class NotesView extends StatelessWidget {
+  const NotesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (context) {
+                return AddNoteButtonSheet();
+              });
+        },
+        child: Icon(Icons.add),
+      ),
+      body: NoteViewBody(),
+    );
+  }
+}
