3b38bc5b60b27d9661452876f1af2502842a9cdd
aalaaabdullah95 <aalaaabdullah835@gmail.com>
2024-07-26 02:23:10 +0300

    create custom textFeild

--- lib/View/notes_view.dart
+++ lib/View/notes_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/View/Widget/add_note_button_sheet.dart';
 
 import 'package:notes_app/View/Widget/note_view_body.dart';
+import 'package:notes_app/constant.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
@@ -10,14 +11,20 @@ class NotesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
+        backgroundColor: kPrimaryColor,
         onPressed: () {
           showModalBottomSheet(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16)),
               context: context,
               builder: (context) {
                 return AddNoteButtonSheet();
               });
         },
-        child: Icon(Icons.add),
+        child: Icon(
+          Icons.add,
+          color: Colors.black,
+        ),
       ),
       body: NoteViewBody(),
     );
