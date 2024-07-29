c292fded3acf025c6e11aa664563c680989f9d9c
aalaaabdullah95 <aalaaabdullah835@gmail.com>
2024-07-29 15:40:27 +0300

    add note cubit

--- lib/View/Widget/custom_button.dart
+++ lib/View/Widget/custom_button.dart
@@ -2,20 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constant.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key});
-
+  const CustomButton({super.key, this.onTap});
+  final void Function()? onTap;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          color: kPrimaryColor, borderRadius: BorderRadius.circular(12)),
-      height: 60,
-      width: MediaQuery.of(context).size.width,
-      child: const Center(
-        child: Text(
-          'Add',
-          style: TextStyle(
-              color: Colors.black, fontSize: 16, fontWeight: FontWeight.bold),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+            color: kPrimaryColor, borderRadius: BorderRadius.circular(12)),
+        height: 60,
+        width: MediaQuery.of(context).size.width,
+        child: const Center(
+          child: Text(
+            'Add',
+            style: TextStyle(
+                color: Colors.black, fontSize: 16, fontWeight: FontWeight.bold),
+          ),
         ),
       ),
     );
